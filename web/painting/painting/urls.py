1c6b8daa418b6f221da66c8a3c606b0e3e70aa53
yeonhwa88 <gidarim0901@naver.com>
2025-03-04 13:08:37 +0900

    siyun0304

--- web/painting/painting/urls.py
+++ web/painting/painting/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
 
 from artist import views
 
@@ -26,3 +28,6 @@ urlpatterns = [
     path('genre/', include('genre.urls')),
     path('style/', include('style.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
